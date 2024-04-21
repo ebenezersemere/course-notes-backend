7d0c39e0290d2357ef80aa6f8ed04783160a1bfe
Amy Liu <Amyliu01@g.hmc.edu>
2024-04-21 15:53:06 -0700

    UserController Test

--- src/test/java/coursenotes/backend/User/UserControllerTest.java
+++ src/test/java/coursenotes/backend/User/UserControllerTest.java
@@ -1,7 +1,116 @@
 package coursenotes.backend.User;
 
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UserControllerTest {
+    @MockBean
+    private UserRepository userRepository;
+    private User mockUser;
+
+    @MockBean
+    private UserService userService;
+
+    @InjectMocks
+    private UserController userController; // Target object to test
+
+    @BeforeEach
+    public void setUp() {
+        userRepository.deleteAll();
+    }
 
     // test that a user is being created
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    public void testCreateUser() throws Exception {
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("testuser");
+        user.setPassword("testpassword");
+
+        Mockito.when(userService.createUser(Mockito.any(User.class))).thenReturn(user);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"username\":\"testuser\",\"password\":\"testpassword\"}"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.username", Matchers.is("testuser")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.password", Matchers.is("testpassword")));
+    }
+
+    @Test
+    public void testReadUser() throws Exception {
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("testuser");
+        user.setPassword("testpassword");
+
+        Mockito.when(userService.readUser(Mockito.anyLong())).thenReturn(user);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/users/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.username", Matchers.is("testuser")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.password", Matchers.is("testpassword")));
+    }
+
+    @Test
+    public void testDeleteUser() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/users/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void testUpdateUser() throws Exception {
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("testuser");
+        user.setPassword("testpassword");
+
+        Mockito.when(userService.updateUser(Mockito.any(User.class))).thenReturn(user);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"id\":1,\"username\":\"testuser\",\"password\":\"testpassword\"}"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.username", Matchers.is("testuser")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.password", Matchers.is("testpassword")));
+    }
+
+    @Test
+    public void testGetUsers() throws Exception {
+        User user1 = new User();
+        user1.setId(1L);
+        user1.setUsername("testuser1");
+        user1.setPassword("testpassword1");
+
+        User user2 = new User();
+        user2.setId(2L);
+        user2.setUsername("testuser2");
+        user2.setPassword("testpassword2");
+
+        List<User> userList = Arrays.asList(user1, user2);
+
+        Mockito.when(userService.getUsers()).thenReturn(userList);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/users"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id", Matchers.is(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].username", Matchers.is("testuser1")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].password", Matchers.is("testpassword1")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].id", Matchers.is(2)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].username", Matchers.is("testuser2")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].password", Matchers.is("testpassword2")));
+    }
+
+
+
 
 }
