822f709caefcc9a4d0d3a78ac4d1e1dd03f580b6
ebenezersemere <ebensemere@icloud.com>
2024-05-01 17:37:25 -0700

    amys test 1

--- src/test/java/coursenotes/backend/User/UserControllerTest.java
+++ src/test/java/coursenotes/backend/User/UserControllerTest.java
@@ -1,5 +1,6 @@
 package coursenotes.backend.User;
 
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import coursenotes.backend.user.User;
 import coursenotes.backend.user.UserRepository;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,6 +21,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 import static org.mockito.Mockito.*;
 
@@ -39,22 +42,28 @@ public class UserControllerTest {
         userRepository.deleteAll();
     }
 
-    @Test // Create
-    public void testAddAuthor() throws Exception {
-        mockUser = new User();
-        mockUser.setFirstName("Amy");
-        when(userRepository.save(any(User.class))).thenReturn(mockUser);
 
+    @Test
+    public void testCreateUser() throws Exception {
+        mockUser = new User();
+        UUID uuid = UUID.fromString("cdd56295-fad7-4fa8-9bd8-3ae80ff8e5a9");
+        mockUser.setUserId(uuid);
+        mockUser.setFirstName("first");
+        mockUser.setLastName("last");
+        mockUser.setEmail("email.com");
+
+        when(userRepository.save(any(User.class))).thenReturn(mockUser);
         String inputJson = new ObjectMapper().writeValueAsString(mockUser);
 
         mockMvc.perform(MockMvcRequestBuilders.post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(inputJson))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("Amy"));
-
+                .andExpect(MockMvcResultMatchers.jsonPath("$.userId").value(uuid.toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("first"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value("last"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("email.com"));
         verify(userRepository, times(1)).save(any(User.class));
-
     }
 
 }
