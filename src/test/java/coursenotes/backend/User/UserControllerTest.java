34f0394a6c5157e6771ad92c7bbe031257354043
ebenezersemere <ebensemere@icloud.com>
2024-05-01 17:23:51 -0700

    one test

--- src/test/java/coursenotes/backend/User/UserControllerTest.java
+++ src/test/java/coursenotes/backend/User/UserControllerTest.java
@@ -1,7 +1,60 @@
 package coursenotes.backend.User;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import coursenotes.backend.user.User;
+import coursenotes.backend.user.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UserControllerTest {
 
-    // test that a user is being created
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    private User mockUser;
+
+    @BeforeEach
+    public void setUp() {
+        userRepository.deleteAll();
+    }
+
+    @Test // Create
+    public void testAddAuthor() throws Exception {
+        mockUser = new User();
+        mockUser.setFirstName("Amy");
+        when(userRepository.save(any(User.class))).thenReturn(mockUser);
+
+        String inputJson = new ObjectMapper().writeValueAsString(mockUser);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(inputJson))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("Amy"));
+
+        verify(userRepository, times(1)).save(any(User.class));
+
+    }
 
 }
